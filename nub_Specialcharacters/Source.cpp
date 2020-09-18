9bc9e3ed208ee3560fa4cabea89ccb2bd6722ce9
WatcharapolxFa <watcharapolyotadee@outlook.com>
2020-09-18 20:16:45 +0700

    Full

--- nub_Specialcharacters/Source.cpp
+++ nub_Specialcharacters/Source.cpp
@@ -1,96 +1,79 @@
-#include <stdio.h>
+﻿#include <stdio.h>
 #include <string.h>
 
-void numbnumber(void);
-int checknum();
+void koko(void);
+void specialcharacters(void);
 
-struct specialcharacters
+
+struct nub_specialcharacters
 {
-    int fa, sum;
-    char sumnum;
-    char num[50];
-}numb;
+    int special , avg ;
+    char sumcharacters;
+    char characters[50];
 
+}nub;
 
 
 
 int main()
 {
-    int check = 0;
-    printf("Input number : ");
-    gets_s(numb.num);
-    numb.fa = strlen(numb.num);
+    koko();
+    printf("Input number: ");
+    gets_s(nub.characters);
 
-    check = checknum();
-
-
-    if (check == numb.fa && numb.num[0] != '\0')
-    {
-
-        printf("OutPut \n ");
-        numbnumber();
-
-    }
-    else
-    {
-        printf("Eror : Input Number !!!");
-    }
+    nub.special = strlen(nub.characters);
+    koko();
+    printf(" OutPut \n ");
+    specialcharacters();
 
 }
 
-int checknum()
+
+
+
+
+void koko()
 {
-    int check = 0;
-    for (int j = 0; j < numb.fa; j++)
-    {
-        for (int i = 0; i < 11; i++) {
-            if (numb.num[j] == ' ' && i == 10)
-            {
-                check++;
-
-            }
-            if (numb.num[j] == 48 + i)
-            {
-                check++;
-
-            }
-        }
-    }
-    return check;
+    printf("\n\n\t\t");
 }
 
 
 
 
 
-void numbnumber()
+
+void specialcharacters()
 {
-    int  check = 0;
+    for (int j = 0; j < nub.special ; j++) {
 
+        nub.sumcharacters = nub.characters[j];
 
-    for (int j = 0; j < numb.fa; j++) {
-
-        numb.sumnum = numb.num[j];
-
-        if (numb.num[j] != ' ' && (numb.num[j] == '0' || numb.num[j] == '1' || numb.num[j] == '2' || numb.num[j] == '3' || numb.num[j] == '4' || numb.num[j] == '5' || numb.num[j] == '6' || numb.num[j] == '7' || numb.num[j] == '8' || numb.num[j] == '9'))
+        if (nub.characters[j] != ' ')
         {
-            numb.sum = 0;
+            nub.avg = 0;
 
 
-            for (int i = 0; i < numb.fa; i++) {
+            for (int i = 0; i < nub.special ; i++) {
 
-                if (numb.sumnum == numb.num[i])
+                if (nub.sumcharacters == nub.characters[i])
                 {
-                    numb.sum++;
-                    numb.num[i] = ' ';
+                    nub.avg++;
+                    nub.characters[i] = ' ';
                 }
-
+                
             }
-            printf(" \t Number : %c = SumNumber %d number.\n", numb.sumnum, numb.sum);
+            koko();
+            printf(" \t Specialcharacters : %c   \n", nub.sumcharacters);
+            koko();
+            printf(" \t With all characters %d body.\n",nub.avg);
+            koko();
+            printf(" \t------------------------------------------\n");
+
         }
+
 
     }
 
 
-
 }
+
